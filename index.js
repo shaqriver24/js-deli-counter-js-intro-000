10ccec80697178e75bc0aa53893dcc499500883b
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-20 07:20:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ function takeANumber(num, name) {
   return `Welcome, ${name}. You are number ${num.length} in line.`
 }
 
-function nowServing(katzDeliLine) {
+/*function nowServing(katzDeliLine) {
   for(var i = katzDeliLine.length; i > 0; katzDeliLine.shift()) {
     return `Currently serving ${katzDeliLine[0]}.`
   }
   return "There is nobody waiting to be served!"
+}*/
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length > 0) {
+    return `Currently serving ${katzDeliLine[0]}.`
+  }
+  else {
+    return "There is nobody waiting to be served!"
+  }
+  katzDeliLine.shift()
 }
