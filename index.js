95c5c765ad19a7990a427f2b0f7f2c18b3f1af82
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-20 06:53:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-/*function takeANumber(num, name) {
+function takeANumber(num, name) {
   num.push(name)
   return `Welcome, ${name}. You are number ${num.length} in line.`
-}*/
-function takeANumber(line, name){
-  var position = line.length+1;
-  line.push(name);
-  return `Welcome, ${name}. You are number ${position} in line.`;
 }
