53d01089b56c34270e59665772447f6d2f424cab
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-20 06:57:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(num, name) {
   num.push(name)
   return `Welcome, ${name}. You are number ${num.length} in line.`
 }
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length > 0) {
+    return katzDeliLine[0]
+  }
+  else {
+    return "There is nobody wating to be served!"
+  }
+}
