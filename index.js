03dc8680c5e95370b684a2f813dfc284ab27c579
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-20 06:44:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(num, name) {
   katzDeli = new Object({num : name})
-  return `Welcome, ${name}. You are number ${num.length + 1} in line.`
+  return `Welcome, ${name}. You are number ${num} in line.`
 }
