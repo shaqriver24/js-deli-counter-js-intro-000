eb28d65c22d9b54ad207f1c13eb68f6c70e7b5ca
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-20 08:04:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,18 +10,16 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!"
 }
 
-function currentLine(num) {
-
-
-  const line = []
-
-  for (let i = 0, 1 = num.length; i < 1; i++) {
-    line.push(`${i + 1}. ${line[i]}`)
-  }
-
-  return `The line is currently: ${line.join(', ')}`
-
-  if (!num.length) {
+function currentLine(line) {
+  if (!line.length) {
     return "The line is currently empty."
   }
+
+const namesNumbers = []
+
+for (let i = 0, l = line.length; i < l; i++) {
+  namesNumbers.push(`${i + 1}. ${line[i]}`)
+}
+
+return `The line is currently: ${namesNumbers.join(', ')}`
 }
