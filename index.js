db2b355dd41b2e729274dc44408cf95f7336969f
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2019-11-20 07:25:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,18 +4,10 @@ function takeANumber(num, name) {
 }
 
 function nowServing(katzDeliLine) {
-  for(var i = katzDeliLine.length; i > 0; katzDeliLine.shift()) {
-    return `Currently serving ${katzDeliLine[0]}.`
-  }
-  return "There is nobody waiting to be served!"
-}
-
-/*function nowServing(katzDeliLine) {
   if(katzDeliLine.length > 0) {
-    katzDeliLine.shift()
-    return `Currently serving ${katzDeliLine[0]}.`
+    return `Currently serving ${katzDeliLine.shift()}.`
   }
   else {
     return "There is nobody waiting to be served!"
   }
-}*/
+}
